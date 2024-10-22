75f321351cede94c990166e726cc70c8ec65dc65
Kalyan Kakati <kalyankakati@yahoo.com>
2024-10-22 15:05:56 -0700

    main.py changed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
-from langchain_openai import OpenAI
+#from langchain_core.prompts import PromptTemplate
+from langchain import PromptTemplate
+from langchain.llms import OpenAI
 
 template = """
     Below is an email that may be poorly worded.
